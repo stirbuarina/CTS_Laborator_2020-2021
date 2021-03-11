3c308a44a180d7bc9665b365e270c3a408abad8b
stirb <stirb@DESKTOP-0QGEL19>
2021-03-11 13:31:06 +0200

    commit sfarsit seminar 3

---
+++ Seminar_3/src/ro/ase/csie/cts/g1088/seminar3/exceptii/ExceptieVechimeClient.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.g1088.seminar3.exceptii;
+
+public class ExceptieVechimeClient extends Exception{
+
+}
---
+++ Seminar_3/src/ro/ase/csie/cts/g1088/seminar3/faza2/TipProdus.java
@@ -0,0 +1,17 @@
+package ro.ase.csie.cts.g1088.seminar3.faza2;
+
+public enum TipProdus {
+	//sunt obiecte in java, nu numere ca in C++
+  NOU(0),DISCOUNT(0.1f),STOC_LIMITAT(0.25f),VECHI(0.35f);
+  
+   private final float discount;
+  
+   private TipProdus(float discount) {
+	   this.discount=discount;
+   }
+   
+   public float getDiscount() {
+	   return this.discount;
+   }
+  
+}
