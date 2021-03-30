672a176beb8328601624bbe72b694ae2083b0837
stirb <stirb@DESKTOP-0QGEL19>
2021-03-30 13:45:56 +0300

    Commit final Singleton si Prototype

--- Laborator5 - Singleton si Prototype/src/ro/ase/csie/cts/dp/singleton/ConexiuneBD.java
+++ Laborator5 - Singleton si Prototype/src/ro/ase/csie/cts/dp/singleton/ConexiuneBD.java
@@ -1,12 +1,13 @@
 package ro.ase.csie.cts.dp.singleton;
 
 public class ConexiuneBD {
-
+	
 	String ip;
 	String denumire;
 	
+	
 	//versiune eager
-	//private static ConexiuneBD conexiune = new ConexiuneBD("10.0.0.1","cts");
+	//private static ConexiuneBD conexiune = new ConexiuneBD("10.0.0.1", "cts");
 	
 	private static ConexiuneBD conexiune = null;
 	
@@ -20,9 +21,10 @@ public class ConexiuneBD {
 		this.denumire = denumire;
 	}
 	
+	
 	//versiune lazy
 	public static synchronized ConexiuneBD getConexiune() {
-		if(conexiune==null) {
+		if(conexiune == null) {
 			//datele se pot prelua din fisiere de configurare
 			conexiune = new ConexiuneBD("10.0.0.1","cts");
 		}
--- Laborator5 - Singleton si Prototype/src/ro/ase/csie/cts/dp/singleton/ModulProfil.java
+++ Laborator5 - Singleton si Prototype/src/ro/ase/csie/cts/dp/singleton/ModulProfil.java
@@ -1,9 +1,8 @@
 package ro.ase.csie.cts.dp.singleton;
 
 public class ModulProfil {
-	
 	public ModulProfil() {
-		//ConexiuneBD conexiune = new ConexiuneBD("10.0.0.1","superGame");
+		//ConexiuneBD conexiune = new ConexiuneBD("10.0.0.1", "superGame");
 		ConexiuneBD conexiune = ConexiuneBD.getConexiune();
 	}
 }
