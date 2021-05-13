80cb3f0c17efdf4111a1f76189c3b3150a23b54e
stirb <stirb@DESKTOP-0QGEL19>
2021-05-13 13:23:35 +0300

    Implementare Command

---
+++ Seminar_11-Command/src/ro/ase/csie/cts/dp/command/InterfataModulJoc.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.dp.command;
+
+public interface InterfataModulJoc {
+
+	public void executaTask(String detalii);
+	
+	
+}
---
+++ Seminar_11-Command/src/ro/ase/csie/cts/dp/command/InterfataTaskAsincron.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.dp.command;
+
+public interface InterfataTaskAsincron {
+
+	public void startTaskAsincron();
+	
+	
+}
