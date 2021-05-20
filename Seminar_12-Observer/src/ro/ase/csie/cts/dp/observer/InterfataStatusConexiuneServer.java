436ea9f12f3ed77388a5b5211e05fe990ab74ed7
stirb <stirb@DESKTOP-0QGEL19>
2021-05-20 12:11:56 +0300

    Implementare Observer

---
+++ Seminar_12-Observer/src/ro/ase/csie/cts/dp/observer/InterfataStatusConexiuneServer.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.dp.observer;
+
+public interface InterfataStatusConexiuneServer {
+
+	
+	public void conexiuneIntrerupta();
+	public void conexiuneActiva();
+	
+}
---
+++ Seminar_12-Observer/src/ro/ase/csie/cts/dp/observer/StareConexiune.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.dp.observer;
+
+public enum StareConexiune {
+ACTIVA,
+INACTIVA
+}
